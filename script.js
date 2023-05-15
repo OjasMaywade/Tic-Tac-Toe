163d5a92f416499dae68e785ce250e055b1e9563
Ojas Maywade <ojasmaywade16@gmail.com>
2023-05-16 00:09:05 +0530

    Implemented: 1. Drop-Down 2.Adding X and O Symbol to box alternatively

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
+let clicked = 0;
+let matrix = [
+    [],
+    [],
+    []
+];
 for(i=0;i<=8;i++){
     document.querySelectorAll(".box")[i].addEventListener("click", function(event){
-        var u = event.target.id;
-        console.log(u);
-        document.querySelectorAll(".box")[u].classList.add("o-symbol");
-        
+        let box_id = event.target.id;
+        if(clicked%2==0){
+        document.querySelectorAll(".box")[box_id].classList.add("o-symbol");
+        matrix[0][box_id]=(box_id);
+        }else{
+        document.querySelectorAll(".box")[box_id].classList.add("x-symbol");
+        }
+        clicked++;
         /*Add code to put O or X in the boxes alternatively and add animation to show hows turn it is.. */
     })
 }
+
     
 
-document.querySelector(".btn").addEventListener("click", function(){
-    alert("Got Clicked");
-   // document.querySelector("#dropdown-content").classList.add("dropdown-content-click")
+document.querySelector("button").addEventListener("click", function(){
+    //alert("Got Clicked");
+   document.querySelector(".dropdown-content").classList.toggle("dropdown-click")
     
 })
 
