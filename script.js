e622828dfb7eb20b6c60d2e4a5f9508ba12d8c2a
Ojas Maywade <ojasmaywade16@gmail.com>
2023-05-18 00:36:54 +0530

    Improved player response storage

--- script.js
+++ script.js
@@ -4,33 +4,44 @@ let matrix = [, , ,];
 for(j=0;j<=2;j++){
    matrix[j] = [, , ,];
 }
-console.log(matrix);
-// Can we create a object or array that can store value of each element according to its preference
+
 // basically we have to create a storage system which will store the response from the users and then compare it to get the result.
-//use mathematics array i and j terms to compare the matrix element
+//use mathematics array m and n terms to compare the matrix element
 // problem with the previous matrix was because of the id of the box
 for(i=0;i<=8;i++){
-    document.querySelectorAll(".boxes>div")[i].addEventListener("click", function(event){
-        let col= event.target.id;
-        let row = event.target.className;
-        console.log(row);
+    document.querySelectorAll(".boxes>div>div")[i].addEventListener("click", function(event){
+        let bId= event.target.id;
+        let col = event.target.className;
+        let row = event.target.parentNode.className;
         if(clicked%2==0){
-        document.querySelectorAll(".boxes>div")[col].classList.add("o-symbol");
+        document.querySelectorAll(".boxes>div>div")[bId].classList.add("o-symbol");
         matrix[row][col]=("o");
         }else{
-        document.querySelectorAll(".boxes>div")[col].classList.add("x-symbol");
+        document.querySelectorAll(".boxes>div>div")[bId].classList.add("x-symbol");
         matrix[row][col]=("x");
         }
         clicked++;
-        console.log(matrix);
+        
+    
         /*Add code to put O or X in the boxes alternatively and add animation to show hows turn it is.. */
     })
 }
+/*
+for(m=0;m<=2;m++){
+    if(matrix[m][0]===matrix[m][1] && matrix[m][1]===matrix[m][2]){
+        alert("Wins");
+    }
+}
+    /*for(n=0;n<=2;n++){
+        if(matrix[0][n]===matrix[1][n] && matrix[1][n]===matrix[2][n]){
+            alert("Wins");
+        }
+    }*/
+
 
     
 
 document.querySelector("button").addEventListener("click", function(){
-    //alert("Got Clicked");
    document.querySelector(".dropdown-content").classList.toggle("dropdown-click")
     
 })
