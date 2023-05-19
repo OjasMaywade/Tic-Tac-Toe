4dfaddd2142345747b4cd336ee745910425ea095
Ojas Maywade <ojasmaywade16@gmail.com>
2023-05-20 02:42:22 +0530

    Fixed Bug for Diagonal element check and losing chance by clicking on occupied blocks

--- script.js
+++ script.js
@@ -11,20 +11,22 @@ for(j=0;j<=2;j++){
 for(i=0;i<=8;i++){
     document.querySelectorAll(".boxes>div>div")[i].addEventListener("click", function(event){
         let bId= event.target.id;
-        let col = event.target.className;
+        let col = event.target.classList;
         let row = event.target.parentNode.className;
-        if(clicked%2==0){
+        if(clicked%2==0 && matrix[row][col[0]] == null){
         document.querySelectorAll(".boxes>div>div")[bId].classList.add("o-symbol");
-        matrix[row][col]=("o");
-        }else{
-        document.querySelectorAll(".boxes>div>div")[bId].classList.add("x-symbol");
-        matrix[row][col]=("x");
-        }
+        matrix[row][col[0]]=("o");
         clicked++;
+        }else if(matrix[row][col[0]] == null){
+        document.querySelectorAll(".boxes>div>div")[bId].classList.add("x-symbol");
+        matrix[row][col[0]]=("x");
+        clicked++;
+        }
+    
         checkWin();
     
-        /*Add code to put O or X in the boxes alternatively and add animation to show hows turn it is.. */
-        /* Improve the code So the user can avoid clicking to occupied box and losing their turn */
+        /*Add code to put O or X in the boxes alternatively (Done) and add animation to show hows turn it is.. */
+        /* Improve the code So the user can avoid clicking to occupied box and losing their turn (Done)*/
     })
 }
 
@@ -43,20 +45,23 @@ function checkWin(){
         }
     }
     }
-    if(matrix[0][0] != null || matrix[1][1] != null || matrix[2][2] != null )
+    if(matrix[0][0] != null || matrix[1][1] != null || matrix[2][2] != null ){
     if(matrix[0][0]===matrix[1][1] && matrix[1][1]===matrix[2][2]){
         let m=1, n = 1;
-        updateScore(m,n);
+        updateScore(1,1);
     }
-    if(matrix[0][2] != null || matrix[1][1] != null || matrix[2][1] != null )
+}
+    if(matrix[0][2] != null || matrix[1][1] != null || matrix[2][1] != null ){
     if(matrix[0][2]===matrix[1][1] && matrix[1][1]===matrix[2][0]){
         let m=1, n = 1;
-        updateScore(m, n);
+        updateScore(1,1);
     }
+}
+    
     /*Write code for checking ties */
 }
 
-function updateScore(){
+function updateScore(m,n){
     if(matrix[m][n]==="x"){
         alert("X Wins");
         document.querySelector(".player-1>h6").innerHTML = 1 + " Win";
@@ -64,6 +69,7 @@ function updateScore(){
         alert("O Wins");
         document.querySelector(".player-2>h6").innerHTML = 1 + " Win";
     }else{
+        alert("Draw");
         document.querySelector(".ties>h6").innerHTML = 1 + " Win";
     }
 }
